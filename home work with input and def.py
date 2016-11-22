dce4fd11f914dc9950824486d3bc659e063008f2
NikictaE <nermohin@gmail.com>
2016-11-22 23:41:19 +0500

    еще тест

--- home work with input and def.py
+++ home work with input and def.py
@@ -94,6 +94,8 @@ def handle(action):
 	
 visits_bank = []
 while True:
+	print('new change')
+	print ("else new change")
 	print (' Введите действие:\n v - ввести новый визит \n p - дата следующего визита, чтобы узнать сколько дней можно будет провести в шенгене \n e - для выхода из приложения \n r - для удаления поездки')
 	schengen_constraint = 180
 	residence_limit = 90
