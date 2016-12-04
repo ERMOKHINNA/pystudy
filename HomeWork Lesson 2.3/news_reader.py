a33b4c857cdd29685db43f5b4394e97befd8fe97
NikictaE <nermohin@gmail.com>
2016-12-04 18:17:40 +0500

    добавил комментарии и подправил вывод

--- HomeWork Lesson 2.3/news_reader.py
+++ HomeWork Lesson 2.3/news_reader.py
@@ -31,6 +31,7 @@ def find_repeated_words(word_list, word_len, word_exception, word_exception_1):
 							count += 1
 				repeated_words[word] = count
 	return (repeated_words)
+# ранжируем слова по частоте использованиея
 def range_repeated_words(repeated_words):
 	ranged_repeated_words = []
 	for v in sorted(repeated_words.values(), reverse = True):
@@ -39,13 +40,13 @@ def range_repeated_words(repeated_words):
 				ranged_repeated_words.append([k, v])
 				repeated_words.pop(k)
 	return (ranged_repeated_words)
-
+# выводим ТОП 10
 def write_result_in_file(filename, result):
 	with open ('outpput.txt', 'a') as out_file:
-		out_file.write('ТОП-10 из файла  %s \n' % (filename))
-		for i in range (20):
+		out_file.write('ТОП-10 из файла  %s \n \n' % (filename))
+		for i in range (10):
 			out_file.write ('%s \n' % (result[i]))
-			
+
 write_result_in_file('newscy.xml',range_repeated_words(find_repeated_words(get_content_from_tag('description', 'newscy.xml', 'KOI8_r'),6, 'href', '<br')))
 write_result_in_file('newsafr.xml',range_repeated_words(find_repeated_words(get_content_from_tag('description', 'newsafr.xml', 'utf-8'),6, 'href', '<br')))
 write_result_in_file('newsfr.xml',range_repeated_words(find_repeated_words(get_content_from_tag('description', 'newsfr.xml', 'iso8859_5'),6, 'href', '<br')))
